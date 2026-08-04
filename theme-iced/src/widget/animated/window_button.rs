3cdf733f90c3fda8257148d8cb8eb1a39ae48ced
KitsuVM <kitsuvm@outlook.com>
2026-08-04 16:33:53 -0300

    feat(kitty-theme): implement animations

---
+++ theme-iced/src/widget/animated/window_button.rs
@@ -0,0 +1,121 @@
+use std::time::Duration;
+
+use iced_anim::{
+    Easing,
+    animated::Mode,
+    widget::{Button, button},
+};
+use iced_core::Element;
+
+pub use crate::widget::window_button::{Catalog, Position, Status, StyleFn, danger};
+
+/// A window button widget to create new buttons.
+pub struct WindowButton<'a, Message, Theme, Renderer>
+where
+    Message: Clone + 'a,
+    Theme: Catalog + button::Catalog + 'a,
+    Renderer: iced_core::renderer::Renderer + 'a,
+{
+    /// The button widget.
+    button_content: Button<'a, Message, Theme, Renderer>,
+    /// The position of the button.
+    position: Position,
+    /// If the window buttons are aligned to the left or right of the window.
+    left_buttons: bool,
+    /// The style function for the button.
+    class: <Theme as Catalog>::Class<'a>,
+    /// If the button should have no rounded corners.
+    no_rounded_corner: bool,
+}
+
+impl<'a, Message, Theme, Renderer> WindowButton<'a, Message, Theme, Renderer>
+where
+    Message: Clone + 'a,
+    Theme: Catalog + button::Catalog + 'a,
+    Renderer: iced_core::renderer::Renderer + 'a,
+{
+    /// Creates a new [`WindowButton`] widget with the given content.
+    pub fn new(content: impl Into<Element<'a, Message, Theme, Renderer>>) -> Self
+    where
+        <Theme as Catalog>::Class<'a>: Into<StyleFn<'a, Theme>>,
+    {
+        Self {
+            button_content: button(content),
+            position: Position::Center,
+            left_buttons: false,
+            class: <Theme as Catalog>::default(),
+            no_rounded_corner: false,
+        }
+    }
+
+    /// Sets the message to send when the button is pressed.
+    pub fn on_press(mut self, message: Message) -> Self {
+        self.button_content = self.button_content.on_press(message);
+        self
+    }
+
+    /// Sets the position of the button.
+    pub fn position(mut self, position: Position) -> Self {
+        self.position = position;
+        self
+    }
+
+    /// Sets whether the window buttons are aligned to the left or right of the window.
+    pub fn left_buttons(mut self, left_buttons: bool) -> Self {
+        self.left_buttons = left_buttons;
+        self
+    }
+
+    /// Sets the style function for the button.
+    pub fn style(mut self, style: impl Fn(&Theme, Status) -> button::Style + 'a) -> Self
+    where
+        <Theme as Catalog>::Class<'a>: From<StyleFn<'a, Theme>>,
+    {
+        self.class = (Box::new(style) as StyleFn<'a, Theme>).into();
+        self
+    }
+
+    /// Sets the class of the button.
+    pub fn class(mut self, class: impl Into<<Theme as Catalog>::Class<'a>>) -> Self {
+        self.class = class.into();
+        self
+    }
+
+    /// Sets whether the button should have no rounded corners.
+    pub fn no_rounded_corner(mut self, no_rounded_corner: bool) -> Self {
+        self.no_rounded_corner = no_rounded_corner;
+        self
+    }
+}
+
+impl<'a, Message, Theme, Renderer> From<WindowButton<'a, Message, Theme, Renderer>>
+    for Element<'a, Message, Theme, Renderer>
+where
+    Message: Clone + 'a,
+    Theme: Catalog + button::Catalog + 'a,
+    Renderer: iced_core::renderer::Renderer + 'a,
+    <Theme as iced_widget::button::Catalog>::Class<'a>:
+        From<iced_widget::button::StyleFn<'a, Theme>>,
+{
+    fn from(window_button: WindowButton<'a, Message, Theme, Renderer>) -> Self {
+        window_button
+            .button_content
+            .style(move |theme: &Theme, status: button::Status| {
+                <Theme as Catalog>::style(
+                    theme,
+                    &window_button.class,
+                    Status {
+                        button_status: status,
+                        button_position: window_button.position,
+                        left_buttons: window_button.left_buttons,
+                        no_rounded_corner: window_button.no_rounded_corner,
+                    },
+                )
+            })
+            .animation(Mode::Transition(Easing {
+                duration: Duration::from_millis(150),
+                ..Default::default()
+            }))
+            .into()
+    }
+}
